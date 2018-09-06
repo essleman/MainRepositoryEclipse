3e552ae4750445ff94fcb8ecc9b05f6a90ce73f4
salas seido <esslemanEss@gmail.com>
2018-09-05 22:36:17 -0700

    It is the first change.

---
+++ NewGitProject/src/main/java/com/NewExsercie/Project/NewGitProject/NewGitProjectApplication.java
@@ -0,0 +1,14 @@
+package com.NewExsercie.Project.NewGitProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewGitProjectApplication.class, args);
+		
+		//This will be my first change for this repository
+	}
+}
---
+++ NewGitProject/src/test/java/com/NewExsercie/Project/NewGitProject/NewGitProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.NewExsercie.Project.NewGitProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewGitProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
